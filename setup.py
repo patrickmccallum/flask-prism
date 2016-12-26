4279bbf3d8b7d345b4a154a9d62f6840a6fa4a41
Patrick McCallum <patrickmccallum49@gmail.com>
2016-12-26 16:09:51 +1000

    Version number incremented

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from distutils.core import setup
 
 setup(
     name='Flask-PRISM',
-    version='0.3.1',
+    version='0.3.2',
     py_modules=['flask_prism'],
     url='http://patsnacks.com/flask-prism',
     license='MIT',
